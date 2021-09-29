4e180bde6dd2986a232a2bd6c9fb06781b35a88f
Maksim Shila <maksimshi@playtika.com>
2021-09-29 15:03:53 +0300

    Fix issue with possible values iterating

--- src/app/strategy/BySinglePossibleValueStrategy.ts
+++ src/app/strategy/BySinglePossibleValueStrategy.ts
@@ -3,19 +3,18 @@ import { SolveStrategy } from './SolveStrategy';
 
 export class BySinglePossibleValueStrategy implements SolveStrategy {
     public solve = (cell: Cell, row: Cell[], column: Cell[], square: Cell[]): boolean => {
-        return cell
-            .getPossibleValues()
-            .map((possibleValue) => {
-                const isSinglePossibleCell =
-                    row.filter((c) => c.getPossibleValues().includes(possibleValue)).length === 1 ||
-                    column.filter((c) => c.getPossibleValues().includes(possibleValue)).length === 1 ||
-                    square.filter((c) => c.getPossibleValues().includes(possibleValue)).length === 1;
-                if (isSinglePossibleCell) {
-                    cell.fill(possibleValue);
-                    return true;
-                }
-                return false;
-            })
-            .some((updated) => updated);
+        const possibleValues = cell.getPossibleValues();
+        for (let i = 0; i < possibleValues.length; i++) {
+            const possibleValue = possibleValues[i];
+            const isSinglePossibleCell =
+                row.filter((c) => c.getPossibleValues().includes(possibleValue)).length === 1 ||
+                column.filter((c) => c.getPossibleValues().includes(possibleValue)).length === 1 ||
+                square.filter((c) => c.getPossibleValues().includes(possibleValue)).length === 1;
+            if (isSinglePossibleCell) {
+                cell.fill(possibleValue);
+                return true;
+            }
+        }
+        return false;
     };
 }
